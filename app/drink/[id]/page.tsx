cbc719af57990c393014cb838508d2478c331ba1
Ruben Marcus <ruben@rubenmarcus.dev>
2024-08-15 07:06:18 +0100

    metadata

--- app/drink/[id]/page.tsx
+++ app/drink/[id]/page.tsx
@@ -1,6 +1,36 @@
+import { Metadata } from "next";
+
 import { DrinkPageComponent } from "@/components/drinkpage";
 import { ROUTES } from "@/config/api";
 import { fetchDrinks } from "@/data/api";
+import { PageParams } from "@/types";
+
+export async function generateMetadata({
+  params,
+}: PageParams): Promise<Metadata> {
+  const id = params.id.split("-")[0];
+  const { drinks } = await fetchDrinks(ROUTES.LOOKUP.COCKTAIL_BY_ID, id);
+
+  return {
+    title: `Cocktail App - ${id}`,
+    openGraph: {
+      title: `Cocktail App - ${id}`,
+      description: drinks[0].strDescription || id,
+      images: [
+        {
+          type: "image/png",
+          url: drinks[0].strDrinkThumb,
+        },
+      ],
+    },
+    twitter: {
+      title: `Cocktail App - ${id}`,
+      description: drinks[0].strDescription || id,
+      card: "summary_large_image",
+      images: drinks[0].strDrinkThumb,
+    },
+  };
+}
 
 export default async function DrinkPage({
   params,
--- app/ingredient/[id]/page.tsx
+++ app/ingredient/[id]/page.tsx
@@ -1,6 +1,39 @@
+import { Metadata } from "next";
+
 import { IngredientPageComponent } from "@/components/ingredientpage";
 import { ROUTES } from "@/config/api";
 import { fetchDrinks } from "@/data/api";
+import { PageParams } from "@/types";
+
+export async function generateMetadata({
+  params,
+}: PageParams): Promise<Metadata> {
+  const id = params.id.split("-")[0];
+  const { ingredients } = await fetchDrinks(
+    ROUTES.SEARCH.INGREDIENT_BY_NAME,
+    id,
+  );
+
+  return {
+    title: `Cocktail App - ${id}`,
+    openGraph: {
+      title: `Cocktail App - ${id}`,
+      description: ingredients[0].strDescription || id,
+      images: [
+        {
+          type: "image/png",
+          url: `https://www.thecocktaildb.com/images/ingredients/${ingredients[0].strIngredient}-Medium.png`,
+        },
+      ],
+    },
+    twitter: {
+      title: `Cocktail App - ${id}`,
+      description: ingredients[0].strDescription || id,
+      card: "summary_large_image",
+      images: `https://www.thecocktaildb.com/images/ingredients/${ingredients[0].strIngredient}-Medium.png`,
+    },
+  };
+}
 
 export default async function IngredientPage({
   params,
